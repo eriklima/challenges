18d06b749d25ea00d9ea3c36175099eb8236b071
Erik Lima <erik.tain@gmail.com>
2024-02-08 10:44:30 -0300

    add sort solution using merge sort

---
+++ 1.medium/array-912-solution.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	seq := []int{9,8,7,6,5,4,3,2,1}
+	fmt.Println(seq)
+
+	res := sort(seq)
+	fmt.Println(res)
+}
+
+func sort(seq []int) []int {
+	mid := len(seq) / 2
+
+	if mid == 0 {
+		return seq
+	}
+
+	left := sort(seq[:mid])
+	right := sort(seq[mid:])
+	return merge(left, right)
+}
+
+func merge(left []int, right []int) []int {
+	l := 0
+	r := 0
+
+	var res []int
+
+	for l < len(left) && r < len(right) {
+		if left[l] <= right[r] {
+			res = append(res, left[l])
+			l++
+		} else {
+			res = append(res, right[r])
+			r++
+		}
+	}
+
+	if l < len(left) {
+		res = append(res, left[l:]...)
+	}
+
+	if r < len(right) {
+		res = append(res, right[r:]...)
+	}
+
+	return res
+}
