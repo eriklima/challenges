d281580a93467bfffe52c4aa94777d142270c3eb
Erik Lima <erik.tain@gmail.com>
2026-08-29 11:25:55 -0300

    all fibonacci

---
+++ JobInterview/all_fibonacci.js
@@ -0,0 +1,98 @@
+// 1 2 3 4 5 6 7 8  9  10
+// 0 1 1 2 3 5 8 13 21 34
+
+// --------------------------------------------------------------
+
+function fib(n) {
+  function fibDoubling(n) {
+    if (n === 0n) {
+      return [0n, 1n];
+    }
+
+    const [a, b] = fibDoubling(n / 2n);
+
+    const c = a * (2n * b - a);
+    const d = a * a + b * b;
+
+    if (n % 2n === 0n) {
+      return [c, d];
+    }
+
+    return [d, c + d];
+  }
+
+  return fibDoubling(BigInt(n))[0];
+}
+
+fib(100);
+
+// --------------------------------------------------------------
+
+function fib(n) {
+  if (n == 0) {
+    console.log(0)
+    return n;
+  }
+  
+  let prev = 0n;
+  let curr = 1n;
+  
+  console.log(0);
+  console.log(1);
+  
+  // for (let i = 2; i <= n; i++) {
+  //   const next = prev + curr;
+  //   prev = curr;
+  //   curr = next;
+  //   console.log(next);
+  // }
+  
+  for (let i = 2; i <= n; i++) {
+    [prev, curr] = [curr, prev + curr];
+    console.log(curr);
+  }
+  
+  return curr;
+}
+
+fib(100);
+
+// --------------------------------------------------------------
+
+const memo = [];
+
+function fib(n) {
+  if (memo[n]) {
+    return memo[n];
+  }
+  
+  if (n <= 1) {
+    memo[0] = 0;      
+    if (n == 1) {
+      memo[1] = 1;
+    }    
+    return n;
+  }
+  
+  memo[n] = fib(n-2) + fib(n-1);
+  
+  return memo[n];
+}
+
+fib(10);
+
+console.log(memo);
+
+// --------------------------------------------------------------
+
+function fib(n) {
+  if (n <= 1) {
+    return n;
+  }
+
+  return fib(n-2) + fib(n-1);
+}
+
+fib(10);
+
+// --------------------------------------------------------------
